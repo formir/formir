8fc3f51092771458a03b51577a760186c2cbab21
Konrad Kubrak <kubrakkonrad@gmail.com>
2023-11-27 22:09:18 +0100

    Add sass translate, other fixes to less

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', function () {
+gulp.task('less', function () {
   return gulp.src('src/less/formir.less')
     .pipe(sourcemaps.init())
     .pipe(less())
@@ -15,9 +15,22 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
+// Sass compile
+// var sass = require('gulp-sass')(require('sass'));
+// sass.compiler = require('node-sass');
+
+// gulp.task('sass', function () {
+//   return gulp.src('./src/sass/formir.scss')
+//     .pipe(sourcemaps.init())
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(postcss([autoprefixer()]))
+//     .pipe(cleanCSS())
+//     .pipe(sourcemaps.write('.'))
+//    .pipe(gulp.dest('./dist/css-sass'));});
+
 gulp.task('assets', function () {
   return gulp.src('src/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('default', gulp.parallel('styles', 'assets'));
+gulp.task('default', gulp.parallel('less', 'assets'));
